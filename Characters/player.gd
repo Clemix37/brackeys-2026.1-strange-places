bdc328413070eb792c5fc13ec6a5267278384656
bmelicque <bmelicque@gmail.com>
2026-02-17 11:31:11 +0100

    Added player animations

--- Characters/player.gd
+++ Characters/player.gd
@@ -2,6 +2,10 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
+@onready var animated_sprite = $AnimatedSprite2D
+
+var lastDir = Vector2.ZERO
+
 func _physics_process(delta: float) -> void:
 	var dir = Vector2.ZERO
 	
@@ -20,5 +24,32 @@ func _physics_process(delta: float) -> void:
 	if isGoingDown:
 		dir.y += 1
 	
+	
 	velocity = dir.normalized() * SPEED
+	
+	if dir.length() == 0:
+		if lastDir.x == 1:
+			animated_sprite.play("idle_right")
+		elif lastDir.x == -1:
+			animated_sprite.play("idle_left")
+		elif lastDir.y == -1:
+			animated_sprite.play("idle_up")
+		elif lastDir.y == 1:
+			animated_sprite.play("idle_down")
+		else:
+			animated_sprite.play("idle_down")
+		
+	else:
+		lastDir = dir
+		if dir.x == 1:
+			animated_sprite.play("run_right")
+		elif dir.x == -1:
+			animated_sprite.play("run_left")
+		elif dir.y == -1:
+			animated_sprite.play("run_up")
+		elif dir.y == 1:
+			animated_sprite.play("run_down")
+		
+	
+	
 	move_and_slide()
