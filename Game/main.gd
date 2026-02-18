aac3adfedf0d95006fa2c35635e838039b4cafce
bmelicque <bmelicque@gmail.com>
2026-02-18 15:08:21 +0100

    Refactored rooms into a single container

---
+++ Game/main.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+signal room_name_changed(room_name: String)
+
+@onready var room_container := $RoomContainer
+@onready var hud := $HUD
+
+var current_room: Node = null
+var is_transitioning = false
+
+func _ready() -> void:
+	load_room("res://Rooms/BreakRoom.tscn")
+	
+func change_room(path: String, room_name: String):
+	print("change room to " + room_name)
+	if is_transitioning: return
+	is_transitioning = true
+	await GameManager.fade_in()
+	load_room(path)
+	await GameManager.fade_out()
+	is_transitioning = false
+	hud.change_room_name(room_name)
+	room_name_changed.emit(room_name)
+
+func load_room(path: String):
+	if current_room: current_room.queue_free()
+	var room_scene = load(path) as PackedScene
+	current_room = room_scene.instantiate()
+	room_container.add_child(current_room)
