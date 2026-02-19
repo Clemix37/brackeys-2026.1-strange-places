1e0508c9bee28644e63bb52f7e7aa6a945f749d1
bmelicque <bmelicque@gmail.com>
2026-02-19 18:34:32 +0100

    Fixed print/pickup task

--- Autoloads/TaskManager.gd
+++ Autoloads/TaskManager.gd
@@ -40,5 +40,5 @@ func add_task(task: Task):
 func _on_task_completed(task: Task):
 	tasks.erase(task.id)
 	if task.next:
-		tasks[task.next.id] = task.next
+		add_task(task.next)
 	updated_tasks.emit()
---
+++ Tasks/pickup_docs.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@onready var task = $PickupDocsTask
+
+func _ready() -> void:
+	TaskManager.updated_tasks.connect(_update_visibility)
+	_update_visibility()
+	task.task_completed.connect(_on_complete_task)
+
+func _update_visibility():
+	$PickupDocsTask.visible = TaskManager.tasks.has(TaskManager.TasksIds.PICKUP_PRINTED_DOCS)
+
+func _on_complete_task(damage):
+	var task = TaskManager.tasks[TaskManager.TasksIds.PICKUP_PRINTED_DOCS]
+	if task: task.complete()
